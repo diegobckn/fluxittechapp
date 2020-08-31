322747ac6267637362eeea254a2bbc573735d523
diegobckn <diego.bckn@gmail.com>
2020-08-31 12:41:00 -0300

    Estilos minimos

--- app/src/main/java/com/diegobckn/fluxit/MainActivity.kt
+++ app/src/main/java/com/diegobckn/fluxit/MainActivity.kt
@@ -1,11 +1,19 @@
 package com.diegobckn.fluxit
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.annotation.RequiresApi
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
+    @RequiresApi(Build.VERSION_CODES.KITKAT)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Objects.requireNonNull(getSupportActionBar())?.setDisplayShowHomeEnabled(true);
+        getSupportActionBar()?.setIcon(R.mipmap.ic_launcher);
+        getSupportActionBar()?.setTitle("");
     }
 }
